e6ec173d4f80313017d36d658daa6ddbfd2f5c81
Nathan Campos <eeepc904@gmail.com>
2021-09-21 02:30:33 +0100

    Started working on the component browser desktop application

---
+++ Browser/Desktop/Forms/MainForm.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Desktop.Forms {
+	/// <summary>
+	/// Main form of the application.
+	/// </summary>
+	public partial class MainForm : Form {
+		/// <summary>
+		/// Initializes the form and its components.
+		/// </summary>
+		public MainForm() {
+			InitializeComponent();
+		}
+	}
+}
