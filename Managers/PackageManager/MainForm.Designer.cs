e8a8abcb7dc594c6271a662ffd83199a18f54b7e
Nathan Campos <eeepc904@gmail.com>
2021-09-28 20:51:36 +0100

    PackageManager - Implemented image editing

--- Managers/PackageManager/MainForm.Designer.cs
+++ Managers/PackageManager/MainForm.Designer.cs
@@ -23,6 +23,7 @@ namespace PackageManager {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.lstPackages = new System.Windows.Forms.ListBox();
 			this.picImage = new System.Windows.Forms.PictureBox();
@@ -32,7 +33,23 @@ namespace PackageManager {
 			this.btnAdd = new System.Windows.Forms.Button();
 			this.btnRemove = new System.Windows.Forms.Button();
 			this.btnRename = new System.Windows.Forms.Button();
+			this.stpMenu = new System.Windows.Forms.MenuStrip();
+			this.packageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.addPackageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.renamePackageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+			this.deletePackageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.imageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.browseImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+			this.deleteImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ctmImage = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.contextBrowseImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextDeleteImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.dlgOpenImage = new System.Windows.Forms.OpenFileDialog();
 			((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+			this.stpMenu.SuspendLayout();
+			this.ctmImage.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// lstPackages
@@ -40,9 +57,9 @@ namespace PackageManager {
 			this.lstPackages.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
 						| System.Windows.Forms.AnchorStyles.Left)));
 			this.lstPackages.FormattingEnabled = true;
-			this.lstPackages.Location = new System.Drawing.Point(9, 9);
+			this.lstPackages.Location = new System.Drawing.Point(9, 28);
 			this.lstPackages.Name = "lstPackages";
-			this.lstPackages.Size = new System.Drawing.Size(200, 381);
+			this.lstPackages.Size = new System.Drawing.Size(200, 355);
 			this.lstPackages.TabIndex = 2;
 			this.lstPackages.SelectedIndexChanged += new System.EventHandler(this.lstPackages_SelectedIndexChanged);
 			// 
@@ -50,16 +67,18 @@ namespace PackageManager {
 			// 
 			this.picImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.picImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.picImage.Location = new System.Drawing.Point(221, 190);
+			this.picImage.ContextMenuStrip = this.ctmImage;
+			this.picImage.Location = new System.Drawing.Point(221, 183);
 			this.picImage.Name = "picImage";
 			this.picImage.Size = new System.Drawing.Size(200, 200);
+			this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.picImage.TabIndex = 3;
 			this.picImage.TabStop = false;
 			// 
 			// lblName
 			// 
 			this.lblName.AutoSize = true;
-			this.lblName.Location = new System.Drawing.Point(218, 9);
+			this.lblName.Location = new System.Drawing.Point(218, 28);
 			this.lblName.Name = "lblName";
 			this.lblName.Size = new System.Drawing.Size(35, 13);
 			this.lblName.TabIndex = 4;
@@ -69,7 +88,7 @@ namespace PackageManager {
 			// 
 			this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
 						| System.Windows.Forms.AnchorStyles.Right)));
-			this.txtName.Location = new System.Drawing.Point(221, 25);
+			this.txtName.Location = new System.Drawing.Point(221, 44);
 			this.txtName.Name = "txtName";
 			this.txtName.Size = new System.Drawing.Size(200, 20);
 			this.txtName.TabIndex = 5;
@@ -77,7 +96,7 @@ namespace PackageManager {
 			// lblID
 			// 
 			this.lblID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.lblID.Location = new System.Drawing.Point(364, 9);
+			this.lblID.Location = new System.Drawing.Point(364, 28);
 			this.lblID.Name = "lblID";
 			this.lblID.Size = new System.Drawing.Size(57, 13);
 			this.lblID.TabIndex = 6;
@@ -87,7 +106,7 @@ namespace PackageManager {
 			// 
 			this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
 						| System.Windows.Forms.AnchorStyles.Right)));
-			this.btnAdd.Location = new System.Drawing.Point(221, 80);
+			this.btnAdd.Location = new System.Drawing.Point(221, 99);
 			this.btnAdd.Name = "btnAdd";
 			this.btnAdd.Size = new System.Drawing.Size(200, 23);
 			this.btnAdd.TabIndex = 9;
@@ -98,17 +117,17 @@ namespace PackageManager {
 			// btnRemove
 			// 
 			this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnRemove.Location = new System.Drawing.Point(323, 51);
+			this.btnRemove.Location = new System.Drawing.Point(323, 70);
 			this.btnRemove.Name = "btnRemove";
 			this.btnRemove.Size = new System.Drawing.Size(98, 23);
 			this.btnRemove.TabIndex = 8;
-			this.btnRemove.Text = "Remove";
+			this.btnRemove.Text = "Delete";
 			this.btnRemove.UseVisualStyleBackColor = true;
 			this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
 			// 
 			// btnRename
 			// 
-			this.btnRename.Location = new System.Drawing.Point(221, 51);
+			this.btnRename.Location = new System.Drawing.Point(221, 70);
 			this.btnRename.Name = "btnRename";
 			this.btnRename.Size = new System.Drawing.Size(98, 23);
 			this.btnRename.TabIndex = 7;
@@ -116,11 +135,120 @@ namespace PackageManager {
 			this.btnRename.UseVisualStyleBackColor = true;
 			this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
 			// 
+			// stpMenu
+			// 
+			this.stpMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.packageToolStripMenuItem,
+            this.imageToolStripMenuItem});
+			this.stpMenu.Location = new System.Drawing.Point(0, 0);
+			this.stpMenu.Name = "stpMenu";
+			this.stpMenu.Size = new System.Drawing.Size(431, 24);
+			this.stpMenu.TabIndex = 10;
+			// 
+			// packageToolStripMenuItem
+			// 
+			this.packageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addPackageToolStripMenuItem,
+            this.renamePackageToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.deletePackageToolStripMenuItem});
+			this.packageToolStripMenuItem.Name = "packageToolStripMenuItem";
+			this.packageToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+			this.packageToolStripMenuItem.Text = "Package";
+			// 
+			// addPackageToolStripMenuItem
+			// 
+			this.addPackageToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("addPackageToolStripMenuItem.Image")));
+			this.addPackageToolStripMenuItem.Name = "addPackageToolStripMenuItem";
+			this.addPackageToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+			this.addPackageToolStripMenuItem.Text = "Add";
+			// 
+			// renamePackageToolStripMenuItem
+			// 
+			this.renamePackageToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("renamePackageToolStripMenuItem.Image")));
+			this.renamePackageToolStripMenuItem.Name = "renamePackageToolStripMenuItem";
+			this.renamePackageToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+			this.renamePackageToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+			this.renamePackageToolStripMenuItem.Text = "Rename";
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(155, 6);
+			// 
+			// deletePackageToolStripMenuItem
+			// 
+			this.deletePackageToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("deletePackageToolStripMenuItem.Image")));
+			this.deletePackageToolStripMenuItem.Name = "deletePackageToolStripMenuItem";
+			this.deletePackageToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Delete)));
+			this.deletePackageToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+			this.deletePackageToolStripMenuItem.Text = "Delete";
+			// 
+			// imageToolStripMenuItem
+			// 
+			this.imageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.browseImageToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.deleteImageToolStripMenuItem});
+			this.imageToolStripMenuItem.Name = "imageToolStripMenuItem";
+			this.imageToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+			this.imageToolStripMenuItem.Text = "Image";
+			// 
+			// browseImageToolStripMenuItem
+			// 
+			this.browseImageToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("browseImageToolStripMenuItem.Image")));
+			this.browseImageToolStripMenuItem.Name = "browseImageToolStripMenuItem";
+			this.browseImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.browseImageToolStripMenuItem.Text = "Browse...";
+			this.browseImageToolStripMenuItem.Click += new System.EventHandler(this.browseImageToolStripMenuItem_Click);
+			// 
+			// toolStripMenuItem2
+			// 
+			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+			this.toolStripMenuItem2.Size = new System.Drawing.Size(149, 6);
+			// 
+			// deleteImageToolStripMenuItem
+			// 
+			this.deleteImageToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("deleteImageToolStripMenuItem.Image")));
+			this.deleteImageToolStripMenuItem.Name = "deleteImageToolStripMenuItem";
+			this.deleteImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.deleteImageToolStripMenuItem.Text = "Delete";
+			this.deleteImageToolStripMenuItem.Click += new System.EventHandler(this.deleteImageToolStripMenuItem_Click);
+			// 
+			// ctmImage
+			// 
+			this.ctmImage.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contextBrowseImageToolStripMenuItem,
+            this.contextDeleteImageToolStripMenuItem});
+			this.ctmImage.Name = "ctmImage";
+			this.ctmImage.Size = new System.Drawing.Size(122, 48);
+			// 
+			// contextBrowseImageToolStripMenuItem
+			// 
+			this.contextBrowseImageToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("contextBrowseImageToolStripMenuItem.Image")));
+			this.contextBrowseImageToolStripMenuItem.Name = "contextBrowseImageToolStripMenuItem";
+			this.contextBrowseImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.contextBrowseImageToolStripMenuItem.Text = "Browse...";
+			this.contextBrowseImageToolStripMenuItem.Click += new System.EventHandler(this.contextBrowseImageToolStripMenuItem_Click);
+			// 
+			// contextDeleteImageToolStripMenuItem
+			// 
+			this.contextDeleteImageToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("contextDeleteImageToolStripMenuItem.Image")));
+			this.contextDeleteImageToolStripMenuItem.Name = "contextDeleteImageToolStripMenuItem";
+			this.contextDeleteImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.contextDeleteImageToolStripMenuItem.Text = "Delete";
+			this.contextDeleteImageToolStripMenuItem.Click += new System.EventHandler(this.contextDeleteImageToolStripMenuItem_Click);
+			// 
+			// dlgOpenImage
+			// 
+			this.dlgOpenImage.Filter = resources.GetString("dlgOpenImage.Filter");
+			this.dlgOpenImage.Title = "Upload Package Image";
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(431, 400);
+			this.ClientSize = new System.Drawing.Size(431, 392);
 			this.Controls.Add(this.btnAdd);
 			this.Controls.Add(this.btnRemove);
 			this.Controls.Add(this.btnRename);
@@ -129,13 +257,18 @@ namespace PackageManager {
 			this.Controls.Add(this.lblName);
 			this.Controls.Add(this.picImage);
 			this.Controls.Add(this.lstPackages);
+			this.Controls.Add(this.stpMenu);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.stpMenu;
 			this.MaximizeBox = false;
 			this.Name = "MainForm";
 			this.Text = "Component Package Manager";
 			this.Load += new System.EventHandler(this.MainForm_Load);
 			((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+			this.stpMenu.ResumeLayout(false);
+			this.stpMenu.PerformLayout();
+			this.ctmImage.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -151,6 +284,20 @@ namespace PackageManager {
 		private System.Windows.Forms.Button btnAdd;
 		private System.Windows.Forms.Button btnRemove;
 		private System.Windows.Forms.Button btnRename;
+		private System.Windows.Forms.MenuStrip stpMenu;
+		private System.Windows.Forms.ToolStripMenuItem packageToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem addPackageToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem renamePackageToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem deletePackageToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem browseImageToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+		private System.Windows.Forms.ToolStripMenuItem deleteImageToolStripMenuItem;
+		private System.Windows.Forms.ContextMenuStrip ctmImage;
+		private System.Windows.Forms.ToolStripMenuItem contextBrowseImageToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem contextDeleteImageToolStripMenuItem;
+		private System.Windows.Forms.OpenFileDialog dlgOpenImage;
 	}
 }
 
--- Managers/PackageManager/MainForm.cs
+++ Managers/PackageManager/MainForm.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -76,9 +77,43 @@ namespace PackageManager {
 			txtName.DataBindings.Add("Text", package, "Name", true,
 				DataSourceUpdateMode.OnPropertyChanged);
 
+			// Populate the image.
+			PopulateImage(package, true);
+
 			// Enable the controls that are now useful.
 			btnRename.Enabled = true;
 			btnRemove.Enabled = true;
+		}
+
+		/// <summary>
+		/// Populates the package image control.
+		/// </summary>
+		/// <param name="package">Currently selected package.</param>
+		/// <param name="retrieve">Should we retrieve the image object?</param>
+		public void PopulateImage(Package package, bool retrieve) {
+			// Clear the image if there's already one in place.
+			if (picImage.Image != null) {
+				picImage.Image.Dispose();
+				picImage.Image = null;
+
+				ctmImage.Enabled = false;
+				imageToolStripMenuItem.Enabled = false;
+			}
+
+			// Retrieve the image object.
+			if (retrieve)
+				package.Retrieve();
+
+			// Associate the image.
+			if (package.Picture.HasImage()) {
+				MemoryStream ms = new MemoryStream();
+
+				ms.Write(package.Picture.FileContent, 0,
+					Convert.ToInt32(package.Picture.FileContent.Length));
+				picImage.Image = System.Drawing.Image.FromStream(ms);
+				
+				ms.Dispose();
+			}
 		}
 
 		/// <summary>
@@ -124,6 +159,45 @@ namespace PackageManager {
 			PopulatePackagesList();
 		}
 
+		/// <summary>
+		/// Browses and selects a package image and uploads it to the server.
+		/// </summary>
+		/// <param name="package">Package that will have its image updated.</param>
+		public void SelectImage(Package package) {
+			// Browse for a new package image.
+			if (dlgOpenImage.ShowDialog() != DialogResult.OK)
+				return;
+
+			// Set the component image and upload the file.
+			package.Picture.AssociatedPackage = new Package(package.ID);
+			package.Picture.FileContent = File.ReadAllBytes(dlgOpenImage.FileName);
+			package.Picture.Save();
+
+			// Show the new image.
+			PopulateImage(package, false);
+		}
+
+		/// <summary>
+		/// Deletes the package image from the form and the server.
+		/// </summary>
+		/// <param name="package">Package that will have its image deleted.</param>
+		public void DeleteImage(Package package) {
+			DialogResult dialog = MessageBox.Show("Are you sure you want to delete " +
+				"this package's image?", "Delete package image?",
+				MessageBoxButtons.YesNo);
+
+			// Ignore if the user was mistaken.
+			if (dialog == DialogResult.No)
+				return;
+
+			// Actually delete the image.
+			package.Picture.Delete();
+			if (picImage.Image != null) {
+				picImage.Image.Dispose();
+				picImage.Image = null;
+			}
+		}
+
 		/******************
 		 * Event Handlers *
 		 ******************/
@@ -158,6 +232,38 @@ namespace PackageManager {
 				return;
 
 			DeletePackage(package);
+		}
+
+		private void browseImageToolStripMenuItem_Click(object sender, EventArgs e) {
+			Package package = (Package)lstPackages.SelectedItem;
+			if (package == null)
+				return;
+
+			SelectImage(package);
+		}
+
+		private void deleteImageToolStripMenuItem_Click(object sender, EventArgs e) {
+			Package package = (Package)lstPackages.SelectedItem;
+			if (package == null)
+				return;
+
+			DeleteImage(package);
+		}
+
+		private void contextBrowseImageToolStripMenuItem_Click(object sender, EventArgs e) {
+			Package package = (Package)lstPackages.SelectedItem;
+			if (package == null)
+				return;
+
+			SelectImage(package);
+		}
+
+		private void contextDeleteImageToolStripMenuItem_Click(object sender, EventArgs e) {
+			Package package = (Package)lstPackages.SelectedItem;
+			if (package == null)
+				return;
+
+			DeleteImage(package);
 		}
 	}
 }
